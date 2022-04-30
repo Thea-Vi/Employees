03970c6901f6d72f68ee68166ad2cf15aa532da2
Thea-Vi <tei.villanueva@gmail.com>
2022-04-30 14:47:21 -0700

    Create and save employee

--- src/main/java/com/villanueva/peopledb/controllers/controller/PeopleController.java
+++ src/main/java/com/villanueva/peopledb/controllers/controller/PeopleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigDecimal;
@@ -29,9 +30,21 @@ public class PeopleController {
         return personRepository.findAll();
     }
 
+    @ModelAttribute
+    public Person getPerson() {
+        return new Person();
+    }
+
     @GetMapping
     public String displayPeople(){
         return "people";
+    }
+
+    @PostMapping
+    public String createPerson(Person person) {
+        System.out.println(person);
+        personRepository.save(person);
+        return "redirect:people";
     }
 }
 
